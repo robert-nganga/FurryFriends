53902f80eeb109fddbacd58943704e264020f7ff
robert-nganga <robertnganga220@gmail.com>
2023-08-18 14:17:40 +0300

    add GetCatById use case

---
+++ app/src/main/java/com/nganga/robert/furryfriends/feature_cat/domain/use_cases/GetCatById.kt
@@ -0,0 +1,22 @@
+package com.nganga.robert.furryfriends.feature_cat.domain.use_cases
+
+import com.nganga.robert.furryfriends.feature_cat.data.mappers.toCat
+import com.nganga.robert.furryfriends.feature_cat.domain.model.Cat
+import com.nganga.robert.furryfriends.feature_cat.domain.repository.CatRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
+import javax.inject.Inject
+
+class GetCatById @Inject constructor(
+    private val catRepository: CatRepository
+) {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(id: String): Flow<List<Cat>>{
+        return catRepository.getCatById(id)
+            .mapLatest { list->
+                list.map { it.toCat() }
+            }
+    }
+}
