efa50e6c4d8d57622fdff0d8114c24769852fbc5
robert-nganga <robertnganga220@gmail.com>
2023-08-25 15:15:41 +0300

    Add GetCatImages use case

--- app/src/main/java/com/nganga/robert/furryfriends/feature_cat/presentation/screens/CatsViewModel.kt
+++ app/src/main/java/com/nganga/robert/furryfriends/feature_cat/presentation/screens/CatsViewModel.kt
@@ -9,9 +9,12 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import coil.imageLoader
 import coil.request.ImageRequest
+import com.nganga.robert.furryfriends.core.util.ResultState
 import com.nganga.robert.furryfriends.feature_cat.domain.model.Cat
+import com.nganga.robert.furryfriends.feature_cat.domain.model.Image
 import com.nganga.robert.furryfriends.feature_cat.domain.use_cases.GetCatBreeds
 import com.nganga.robert.furryfriends.feature_cat.domain.use_cases.GetCatById
+import com.nganga.robert.furryfriends.feature_cat.domain.use_cases.GetCatImages
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
@@ -21,7 +24,8 @@ import javax.inject.Inject
 @HiltViewModel
 class CatsViewModel@Inject constructor(
     getCatBreeds: GetCatBreeds,
-    private val getCatById: GetCatById
+    private val getCatById: GetCatById,
+    private val getCatImages: GetCatImages
 ) : ViewModel() {
 
     val allBreeds = getCatBreeds()
@@ -30,10 +34,28 @@ class CatsViewModel@Inject constructor(
     var cat by mutableStateOf(Cat())
         private set
 
+    var catImages by mutableStateOf<List<Image>>(emptyList())
+        private  set
+
     fun fetchCatById(id:String) = viewModelScope.launch(Dispatchers.IO){
         getCatById(id).collectLatest { results->
             if(results.isNotEmpty()){
                 cat = results.first()
+            }
+        }
+    }
+
+    fun fetchCatImages(id:String) = viewModelScope.launch(Dispatchers.IO){
+        val result = getCatImages(id)
+        when(result.status){
+            ResultState.Status.SUCCESS->{
+                result.data?.let {
+                    catImages = it
+                }
+            }
+            ResultState.Status.LOADING->{}
+            ResultState.Status.ERROR ->{
+
             }
         }
     }
